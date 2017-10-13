e5d24378dea337674f42dc22ca9b01065bc18566
olga-nest <olga.w.nesterova@gmail.com>
2017-10-12 20:19:29 -0700

    Add pet method and a bucket imageView

--- Phonagotchi/LPGViewController.m
+++ Phonagotchi/LPGViewController.m
@@ -7,10 +7,13 @@
 //
 
 #import "LPGViewController.h"
+#import "ONEPet.h"
 
 @interface LPGViewController ()
 
 @property (nonatomic) UIImageView *petImageView;
+@property (nonatomic, strong) ONEPet *myPet;
+@property (nonatomic) UIImageView *bucketImageView;
 
 @end
 
@@ -20,7 +23,11 @@
 {
     [super viewDidLoad];
 	
+    ONEPet *myPet = [ONEPet new];
+    
     self.view.backgroundColor = [UIColor colorWithRed:(252.0/255.0) green:(240.0/255.0) blue:(228.0/255.0) alpha:1.0];
+    
+    //Pet view
     
     self.petImageView = [[UIImageView alloc] initWithFrame:CGRectZero];
     self.petImageView.translatesAutoresizingMaskIntoConstraints = NO;
@@ -45,6 +52,64 @@
                                  multiplier:1.0
                                    constant:0.0].active = YES;
     
+    [self.petImageView setUserInteractionEnabled:YES];
+    
+    //Bucket view
+    
+    self.bucketImageView = [[UIImageView alloc]initWithFrame:CGRectZero];
+    self.bucketImageView.translatesAutoresizingMaskIntoConstraints = NO;
+    
+    self.bucketImageView.image = [UIImage imageNamed:@"bucket"];
+    [self.view addSubview:self.bucketImageView];
+    
+    [NSLayoutConstraint constraintWithItem:self.bucketImageView
+                                 attribute:NSLayoutAttributeLeading
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.view
+                                 attribute:NSLayoutAttributeLeading
+                                multiplier:1.0
+                                  constant:30.0].active = YES;
+    
+    [NSLayoutConstraint constraintWithItem:self.bucketImageView
+                                 attribute:NSLayoutAttributeBottomMargin
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:self.view
+                                 attribute:NSLayoutAttributeBottomMargin
+                                multiplier:1.0
+                                  constant:-30.0].active = YES;
+    
+    [NSLayoutConstraint constraintWithItem: self.bucketImageView
+                                 attribute:NSLayoutAttributeWidth
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:nil
+                                 attribute:NSLayoutAttributeWidth
+                                multiplier:1
+                                  constant:100]. active = YES;
+    
+    [NSLayoutConstraint constraintWithItem: self.bucketImageView
+                                 attribute:NSLayoutAttributeHeight
+                                 relatedBy:NSLayoutRelationEqual
+                                    toItem:nil
+                                 attribute:NSLayoutAttributeHeight
+                                multiplier:1
+                                  constant:100]. active = YES;
+    
+    
+    [self.bucketImageView setUserInteractionEnabled:YES];
+    
+    
+    
+    // Pet the cat
+    UIPanGestureRecognizer *panRecognizer = [[UIPanGestureRecognizer alloc]initWithTarget:self action:@selector(petTheCat:)];
+    [self.petImageView addGestureRecognizer:panRecognizer];
+    
+}
+
+-(void)petTheCat: (UIPanGestureRecognizer *) sender {
+    CGPoint petVelocity =  [sender velocityInView:self.petImageView];
+    if (petVelocity.x < 5000 || petVelocity.y < 5000) {
+    self.petImageView.image=[UIImage imageNamed:@"grumpy.png"];
+    }
 }
 
 @end
